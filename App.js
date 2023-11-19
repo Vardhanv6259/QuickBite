c40ee00eda41f5fd0ff174f55eec84e0741e3ef4
Vardhanv6259 <vardhanv6259@gmail.com>
2023-11-19 16:34:41 +0530

    initial site

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = (
+  <div>
+    <h1>this is sample</h1>
+  </div>
+);
+// console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
